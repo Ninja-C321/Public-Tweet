2795741caf70ca803c6ffce977e6b9aaecc3ea5e
Ninja-C321 <umeshbadsara321@gmail.com>
2025-10-19 20:29:54 +0530

    Update views.py

--- tweet/public_tweet/views.py
+++ tweet/public_tweet/views.py
@@ -24,28 +24,8 @@ def tweet_create(request):
         form = TweetForm()
     return render(request, 'tweet_form.html', {'form': form})
 
-def tweet_edit(request, tweet_id):
-    tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
-    if request.method == 'POST':
-         form = TweetForm(request.POST , request.FILES, instance=tweet)
-         if form.is_valid():
-             tweet = form.save(commit=False)
-             tweet.user = request.user
-             tweet = form.save()
-             return redirect('tweet_list')
 
-    else :
-        form = TweetForm(instance=tweet)
-    return render(request, 'tweet_form.html', {'form': form})
-
-
-def tweet_delete(request, tweet_id):
-    tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
-    
-    if request.method == 'POST':
-        tweet.delete()
-        return redirect('tweet_list')
-    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
     
         
+
         
